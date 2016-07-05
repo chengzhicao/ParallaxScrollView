853effb4fd900bef691001f6505454af3e227c0c
ChengZhiCao <chenglove1201@163.com>
2016-07-05 11:13:19 +0800

    1.增加轮播指示器

--- app/src/main/java/com/icandemy/parallaxscrollview/MainActivity.java
+++ app/src/main/java/com/icandemy/parallaxscrollview/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private InfiniteLoopView infiniteLoopView;
     private List<View> imageList;
+    private List<ImageView> image_indication_list;
+    private LinearLayout linearLayout;
+    /**
+     * 指示器边距
+     */
+    private final int INDICATION_MARGIN = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +31,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.bar_toolbar);
-        toolbar.getBackground().setAlpha(0);
         infiniteLoopView = (InfiniteLoopView) findViewById(R.id.viewpager_ads);
         parallaxScrollView = (ParallaxScrollView) findViewById(R.id.icandemy_scroller);
+        linearLayout = (LinearLayout) findViewById(R.id.linear_indication);
 
+        initView();
+    }
+
+    /**
+     * 初始化界面
+     */
+    private void initView() {
+        toolbar.getBackground().setAlpha(0);
         parallaxScrollView.setScrollViewControl(toolbar, infiniteLoopView);//设置toolbar渐变
 
         imageList = new ArrayList<View>();
@@ -33,6 +50,20 @@ public class MainActivity extends AppCompatActivity {
         imageList.add(View.inflate(this, R.layout.pager_ads_second, null));
         imageList.add(View.inflate(this, R.layout.pager_ads_third, null));
         imageList.add(View.inflate(this, R.layout.pager_ads_fourth, null));
+
+        image_indication_list = new ArrayList<ImageView>();
+        for (int i = 0; i < imageList.size(); i++) {//初始化指示器
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(R.drawable.loop_indicator);
+            imageView.setPadding(INDICATION_MARGIN, INDICATION_MARGIN, INDICATION_MARGIN, INDICATION_MARGIN);
+            if (i == 0) {
+                imageView.setAlpha(1.0f);
+            } else {
+                imageView.setAlpha(0.5f);
+            }
+            image_indication_list.add(imageView);
+            linearLayout.addView(imageView);
+        }
 
         infiniteLoopView.setAdapter(new ViewPagerAdapter(imageList));
         infiniteLoopView.setCurrentItem(ViewPagerAdapter.CURRENT_VALUE);//设置当前轮播图
@@ -45,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 infiniteLoopView.setCurrentItem(position);//每当手动滑动轮播图后，更新当前current值
+                setIndication(position);
+                Log.d("MainActivity", "position:" + position);
             }
 
             @Override
@@ -52,5 +85,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    /**
+     * 指示器设置
+     *
+     * @param position
+     */
+    private void setIndication(int position) {
+        switch (position % 4) {
+            case 0:
+                image_indication_list.get(0).setAlpha(1.0f);
+                image_indication_list.get(1).setAlpha(0.5f);
+                image_indication_list.get(2).setAlpha(0.5f);
+                image_indication_list.get(3).setAlpha(0.5f);
+                break;
+            case 1:
+                image_indication_list.get(0).setAlpha(0.5f);
+                image_indication_list.get(1).setAlpha(1.0f);
+                image_indication_list.get(2).setAlpha(0.5f);
+                image_indication_list.get(3).setAlpha(0.5f);
+                break;
+            case 2:
+                image_indication_list.get(0).setAlpha(0.5f);
+                image_indication_list.get(1).setAlpha(0.5f);
+                image_indication_list.get(2).setAlpha(1.0f);
+                image_indication_list.get(3).setAlpha(0.5f);
+                break;
+            case 3:
+                image_indication_list.get(0).setAlpha(0.5f);
+                image_indication_list.get(1).setAlpha(0.5f);
+                image_indication_list.get(2).setAlpha(0.5f);
+                image_indication_list.get(3).setAlpha(1.0f);
+                break;
+            default:
+                break;
+        }
     }
 }
